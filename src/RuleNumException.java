4991d5ce20fc5888cd7935b801b03526684e1a05
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2022-03-10 12:52:42 -0600

    Added JavaDoc for ctor RuleNumException

--- src/RuleNumException.java
+++ src/RuleNumException.java
@@ -5,7 +5,11 @@ public class RuleNumException extends Exception{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+	/**
+	 * Sets the exception message using a given range
+	 * @param min minimum value of range
+	 * @param max maximum value of range
+	 */
 	public RuleNumException(int min,int max){
 	 super("ruleNum is outside the range [" + min +", "+ max+"].");
 	}
