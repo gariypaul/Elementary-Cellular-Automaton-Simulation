843c9774b7b0dad7d5387ae395c1cd2c69e36c2a
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2022-03-10 10:16:53 -0600

    Created RuleNumException.java and ctor

---
+++ src/RuleNumException.java
@@ -0,0 +1,13 @@
+
+public class RuleNumException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public RuleNumException(int min,int max){
+		//TODO: Finish constructor
+	}
+
+}
