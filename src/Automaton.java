9b9aadedf2bf7e87fa3c8b75bc987a8d8d08909f
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2022-03-10 10:03:41 -0600

    Added incomplete method framework Automaton

--- src/Automaton.java
+++ src/Automaton.java
@@ -1,4 +1,46 @@
+import java.util.ArrayList;
 
-public class Automaton {
-
+public abstract class Automaton {
+	private Rule rule;
+	private ArrayList <Generation> generations;
+	public char falseSymbol = '0';
+	public char trueSymbol = '1';
+	
+	protected Automaton(int ruleNum, Generation initial) {
+		
+	}
+	protected Automaton(String filename) {
+		
+	}
+	public int evolve (int numSteps) {
+		return numSteps;//fix mee
+		
+	}
+	public Generation getGeneration(int stepNum) {
+		return null;//fix mee
+		
+	}
+	public int getRuleNum() {
+		return 1;//fixmee
+		
+	}
+	public int getTotalSteps() {
+		return 0;//fix mee
+	}
+	public void saveEvolution() {
+		
+	}
+	@Override
+	public String toString() {
+		return "Automaton";//fix mee
+	}
+	public String ruleTableString() {
+		return "Automaton";//fix mee
+	}
+	protected abstract Rule createRule(int ruleNum);
+	
+	public static Automaton createAutomaton(CellularAutomaton ca, int ruleNum, Generation initial) {
+		return null;//fix mee
+		
+	}
 }
