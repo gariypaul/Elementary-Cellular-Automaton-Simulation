53ba4375e5c4a89697cfa1ecf8403b0508120cd8
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2022-03-10 10:29:05 -0600

    Created and Added methods ElementaryRule

---
+++ src/ElementaryRule.java
@@ -0,0 +1,27 @@
+
+public class ElementaryRule extends Rule{
+
+	protected ElementaryRule(int ruleNum) {
+		super(ruleNum);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean[] getNeighborhood(int idx, Generation gen) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean evolve(boolean[] neighborhood) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String ruleTableString(char falseSymbol, char trueSymbol) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
