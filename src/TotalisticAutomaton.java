700b9d90eec6ba62b84ed62b7c429131174dcb3b
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2022-03-10 10:24:58 -0600

    Created and Added Methods TotalisticAutomaton

---
+++ src/TotalisticAutomaton.java
@@ -0,0 +1,17 @@
+
+public class TotalisticAutomaton extends Automaton {
+
+	protected TotalisticAutomaton(int ruleNum, Generation initial) {
+		super(ruleNum, initial);
+		// TODO Auto-generated constructor stub
+	}
+	protected TotalisticAutomaton(String filename) {
+		super(filename);
+	}
+	@Override
+	protected Rule createRule(int ruleNum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
