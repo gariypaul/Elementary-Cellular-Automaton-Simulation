a052f0c1b87c0dc1b4d36f9c9e09809f201d5c13
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2022-03-10 13:44:43 -0600

    Implemented getneighBorhood bby radius method

--- src/Rule.java
+++ src/Rule.java
@@ -6,16 +6,7 @@ public abstract class Rule{
 	 * @param ruleNum the rule number that the Rule object is constructed with
 	 */
 	protected Rule(int ruleNum) {
-		//check if ruleNum is within rule number bounds otherwise set it to closes rule number
-		if(ruleNum>=0&&ruleNum<=255){ 
 			this.ruleNum=ruleNum;
-		}
-		else if(ruleNum<0) {
-			this.ruleNum=0;
-		}
-		else {
-			this.ruleNum=255;
-		}
 	}
 	
 	/**
@@ -47,9 +38,29 @@ public abstract class Rule{
 		Generation newGen = new Generation(states);
 		return newGen;
 	}
-	
+	/**
+	 * Gets the neighborhood of a given index in the generation with a given radius e.g index of radius one [cellStates[idx-1], cellStates[idx], cellStates[idx+1]]
+	 * @param idx this is the index that is the center of the neighborhood
+	 * @param radius this is the number of indices that are to be found to the left and right of the given index
+	 * @param gen this is the generation that contains the cell states 
+	 * @return returns a boolean array that contains the given index state at the center and the neighboring cell states to the left and right
+	 */
 	public static boolean[] getNeighborhoodByRadius(int idx,int radius,Generation gen) {
-		return null;//fix mee
-		
+		boolean[] neighborhoodBool = new boolean[1+(radius*2)];
+		if(!(idx==0)&&idx!=gen.size()-1) {
+			for(int i=0,j=idx-radius;i<neighborhoodBool.length&&j<idx+radius+1;i++,j++) {
+				if(j<0) {
+					neighborhoodBool[i]=gen.getState(gen.size()+j);
+				}
+				else if(j>=gen.size()) {
+					neighborhoodBool[i]=gen.getState(j-gen.size());
+				}
+				else {
+					neighborhoodBool[i]=gen.getState(j);
+				}
+				
+			}
+		}
+		return neighborhoodBool;		
 	} 
 }
