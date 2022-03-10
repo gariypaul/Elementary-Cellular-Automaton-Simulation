bde0f8d90821ed152f3a81d72a5b141565c986b5
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2022-03-10 09:52:59 -0600

    Added incomplete method framework

--- src/Generation.java
+++ src/Generation.java
@@ -1,4 +1,26 @@
 
 public class Generation {
-
+	private boolean[] cellStates;
+	public Generation(boolean[] states) {
+		
+	}
+	public Generation(boolean[] states, char trueSymbol) {
+		
+	}
+	public boolean getState (int idx) {
+		return false;//Fix mee
+		
+	}
+	public boolean getStates() {
+		return false;//Fix mee
+		
+	}
+	public String getStates(char falseSymbol,char trueSymbol) {
+		return null;//Fix mee
+		
+	}
+	public int size() {
+		return 0;//Fix mee
+		
+	}
 }
