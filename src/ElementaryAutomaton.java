03f11a98b733a315261cf512482661fac22c00b4
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2022-03-10 10:26:22 -0600

    Created and Added methods ElementaryAutomaton

---
+++ src/ElementaryAutomaton.java
@@ -0,0 +1,18 @@
+
+public class ElementaryAutomaton extends Automaton {
+
+	protected ElementaryAutomaton(int ruleNum, Generation initial) {
+		super(ruleNum, initial);
+		// TODO Auto-generated constructor stub
+	}
+	
+	protected ElementaryAutomaton(String filename) {
+		super(filename);
+	}
+	@Override
+	protected Rule createRule(int ruleNum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
