d858e22f1b39b8c8b9ec32c30e1a9aaf87520fb7
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2022-03-10 10:29:40 -0600

    Created and added methods TotalisticRule

---
+++ src/TotalisticRule.java
@@ -0,0 +1,27 @@
+
+public class TotalisticRule extends Rule {
+
+	protected TotalisticRule(int ruleNum) {
+		super(ruleNum);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean[] getNeighborhood(int idx, Generation gen) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean evolve(boolean[] neighborhood) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String ruleTableString(char falseSymbol, char trueSymbol) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
