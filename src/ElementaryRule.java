79c1c5ae82094edfbfeeaa3f1b104faea735370a
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2022-03-10 17:04:21 -0600

    Implemented getNeighborhood method

--- src/ElementaryRule.java
+++ src/ElementaryRule.java
@@ -1,20 +1,24 @@
+import java.util.Arrays;
 
 public class ElementaryRule extends Rule{
-
+	/**
+	 * Constructor for new ElementaryRule Object
+	 * @param ruleNum this is the rule number to be set
+	 */
 	protected ElementaryRule(int ruleNum) {
 		super(ruleNum);
-		// TODO Auto-generated constructor stub
 	}
-
+	/**
+	 * This method returns a boolean array with the state of the index specified and the neighbors to a radius one on the left and right
+	 */
 	@Override
 	public boolean[] getNeighborhood(int idx, Generation gen) {
-		// TODO Auto-generated method stub
-		return null;
+		return Arrays.copyOf(super.getNeighborhoodByRadius(idx, 1, gen),super.getNeighborhoodByRadius(idx, 1, gen).length);
 	}
-
+	
 	@Override
 	public boolean evolve(boolean[] neighborhood) {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
