f9d586e2b9ef97b6d368a7d624c1230843555df7
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2022-03-10 10:38:00 -0600

    Implemenetd Generation(States) ctor

--- src/Generation.java
+++ src/Generation.java
@@ -1,9 +1,20 @@
+import java.util.Arrays;
 
 public class Generation {
-	private boolean[] cellStates;
-	public Generation(boolean[] states) {
-		
+	private boolean[] cellStates= new boolean[1];
+	/**
+	 * Generates status array from specified boolean states
+	 * @param states the states that are specified by user of method
+	 */
+	public Generation (boolean... states){
+		if(states!=null&&states.length>0) {
+			this.cellStates=Arrays.copyOf(states, states.length);
+		}
+		else{
+			this.cellStates[0]=false;
+		}
 	}
+	
 	public Generation(boolean[] states, char trueSymbol) {
 		
 	}
