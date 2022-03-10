bdf83c8550d131952004834e697e7a0b681ace5b
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2022-03-10 10:16:04 -0600

    Added incomplete method framework Rule

--- src/Rule.java
+++ src/Rule.java
@@ -1,4 +1,27 @@
 
-public class Rule {
-
+public abstract class Rule{
+	private int ruleNum;
+	
+	protected Rule(int ruleNum) {
+		// TODO Auto-generated constructor stub
+	}
+	public int getRuleNum() {
+		return ruleNum;//complete mee
+	}
+	public abstract boolean[] getNeighborhood(int idx, Generation gen);
+	
+	public boolean evolve (boolean[] neighborhood) {
+		return false;//fix mee
+	}
+	public Generation evolve(Generation gen) {
+		return gen;//fix me
+		
+	} 
+	public String ruleTableString(char falseSymbol, char trueSymbol) {
+		return "rule";//fix mee
+	}
+	public static boolean[] getNeighborhoodByRadius(int idx,int radius,Generation gen) {
+		return null;//fix mee
+		
+	} 
 }
