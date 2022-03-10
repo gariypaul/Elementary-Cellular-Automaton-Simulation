842a4d2d25c634361c803ca57a8442bc2b03b9d0
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2022-03-10 10:48:59 -0600

    Implemeted Rule(ruleNum) ctor

--- src/Rule.java
+++ src/Rule.java
@@ -1,25 +1,35 @@
 
 public abstract class Rule{
-	private int ruleNum;
-	
+	private int ruleNum=0;
+	/**
+	 * Constructor method that initializes a rule object with the rule number as input
+	 * @param ruleNum the rule number that the Rule object is constructed with
+	 */
 	protected Rule(int ruleNum) {
-		// TODO Auto-generated constructor stub
+		//check if ruleNum is within rule number bounds otherwise set it to closes rule number
+		if(ruleNum>=0&&ruleNum<=255){ 
+			this.ruleNum=ruleNum;
+		}
+		else if(ruleNum<0) {
+			this.ruleNum=0;
+		}
+		else {
+			this.ruleNum=255;
+		}
 	}
 	public int getRuleNum() {
 		return ruleNum;//complete mee
 	}
 	public abstract boolean[] getNeighborhood(int idx, Generation gen);
 	
-	public boolean evolve (boolean[] neighborhood) {
-		return false;//fix mee
-	}
+	public abstract boolean evolve (boolean[] neighborhood);
+	
 	public Generation evolve(Generation gen) {
 		return gen;//fix me
 		
 	} 
-	public String ruleTableString(char falseSymbol, char trueSymbol) {
-		return "rule";//fix mee
-	}
+	public abstract String ruleTableString(char falseSymbol, char trueSymbol);
+
 	public static boolean[] getNeighborhoodByRadius(int idx,int radius,Generation gen) {
 		return null;//fix mee
 		
