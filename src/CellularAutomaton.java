964383806fc2ef02c1bef5fd456625114aa226b5
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2022-03-10 23:12:29 -0600

    Added JavaDoc CellularAutomaton

--- src/CellularAutomaton.java
+++ src/CellularAutomaton.java
@@ -3,7 +3,12 @@ public enum CellularAutomaton {
 
 	ECA,
 	TCA;
-	
+	/**
+	 * This returns the type of CellularAtomaton that is to be used to generate an Automaton object
+	 * @param s this contains the type of Automaton i.e "ECA" or "TCA"
+	 * @return it returns the type of Automaton to be used
+	 * @throws CellularAutomatonNotFoundException this is thrown when the CellularAutomaton is not either "ECA" or "TCA"
+	 */
 	public static CellularAutomaton parse(String s) throws CellularAutomatonNotFoundException {
 		if (s.equalsIgnoreCase("ECA")) { 
 			return ECA;
