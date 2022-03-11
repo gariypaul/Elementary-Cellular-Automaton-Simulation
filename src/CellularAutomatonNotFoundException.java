445fcc9b7e40e422fd201b8ecc32a31c31700a05
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2022-03-10 23:14:18 -0600

    Added JavaDoc CellularAutomatonNotFoundException

--- src/CellularAutomatonNotFoundException.java
+++ src/CellularAutomatonNotFoundException.java
@@ -2,7 +2,10 @@
 public class CellularAutomatonNotFoundException extends Exception {
 
 	private static final long serialVersionUID = 1L;
-	
+	/**
+	 * This is an exception thrown when the CellularAutomaton specified in CellularAutomaton enum is unknown
+	 * @param s this is the unknown CellularAutomaton that is passed through CellularAutomaton enum
+	 */
 	public CellularAutomatonNotFoundException(String s) {
 		super("Unknown cellular automaton type " + s);
 	}
