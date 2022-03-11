a86256e272ec9866ced3e5e01342f1d0dd5f1404
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2022-03-10 21:48:16 -0600

    Implemented ctor method and Inherited abstract methods TotalisticAutomaton

--- src/TotalisticAutomaton.java
+++ src/TotalisticAutomaton.java
@@ -1,17 +1,18 @@
+import java.io.FileNotFoundException;
 
 public class TotalisticAutomaton extends Automaton {
 
-	protected TotalisticAutomaton(int ruleNum, Generation initial) {
+	protected TotalisticAutomaton(int ruleNum, Generation initial) throws RuleNumException {
 		super(ruleNum, initial);
 		// TODO Auto-generated constructor stub
 	}
-	protected TotalisticAutomaton(String filename) {
+	protected TotalisticAutomaton(String filename) throws FileNotFoundException, RuleNumException {
 		super(filename);
 	}
 	@Override
-	protected Rule createRule(int ruleNum) {
+	protected Rule createRule(int ruleNum) throws RuleNumException {
 		// TODO Auto-generated method stub
-		return null;
+		return new TotalisticRule(ruleNum);
 	}
 
 }
